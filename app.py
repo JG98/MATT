b7cd83f3804861426efe2414904e63d732a86699
JG98 <jeffgower98@gmail.com>
2020-01-08 14:27:39 +0100

    Added database support

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask, make_response, render_template, request, session
 from base64 import b64decode
 from files.tree import Tree
-from pymongo import MongoClient
-from uuid import uuid4
+import sqlite3
 
 app = Flask(__name__, static_url_path="/static")
 app.secret_key = b'H.\xf8\xd7|J\x98\x16/(\x86\x05X\xf8")\x11\x9dM\x08\xcc\xfe\xa2\x03'
-
-client = MongoClient()
-db = client.database
-collection = db.collection
 
 @app.route("/")
 def home():
@@ -22,20 +17,20 @@ def home():
 
 @app.route("/load", methods=["POST", "GET"])
 def load():
+    conn = sqlite3.connect('trees.db')
+    c = conn.cursor()
     if request.method == "POST":
-        #uuid = str(uuid4())
-        #tree = Tree(b64decode(request.form.get("file").split("base64,")[1]).decode()).to_json()
-        #post_id = collection.insert_one({"id": uuid, "tree": tree})
-        # TODO localStorage in JS or flask_session or pymongo
-        session["tree"] = Tree(b64decode(request.form.get("file").split("base64,")[1]).decode()).to_json()
-        print(session["tree"])
+        tree = Tree(b64decode(request.form.get("file").split("base64,")[1]).decode()).to_json()
+        c.execute('''INSERT INTO trees (json) VALUES (?)''', [tree])
+        session["tree"] = c.lastrowid
     elif request.method == "GET":
-        # TODO localStorage in JS or flask_session or pymongo
-        session["tree"] = Tree(session["tree"], request.args.get("from"), request.args.get("to")).to_json()
+        c.execute('''SELECT json FROM trees WHERE id = ?''', [session["tree"]])
+        tree = Tree(c.fetchone()[0], request.args.get("from"), request.args.get("to")).to_json()
     else:
         pass # TODO
-    # TODO localStorage in JS or flask_session or pymongo
-    response = make_response(session["tree"])
+    conn.commit()
+    conn.close()
+    response = make_response(tree)
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, max-age=0"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
@@ -46,3 +41,9 @@ if __name__ == '__main__':
     #app.run(host='127.0.0.1', port=80)
     #app.run(host='0.0.0.0', port=80)
     app.run()
+
+    conn = sqlite3.connect('trees.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS trees (id INTEGER PRIMARY KEY AUTOINCREMENT, json TEXT)''')
+    conn.commit()
+    conn.close()
