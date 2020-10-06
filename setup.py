54fc7f1846f978d8d850238a81e36afabc0fa5f7
JG98 <jeffgower98@gmail.com>
2020-10-06 12:13:47 +0200

    PyPi upload prep

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bio-matt-test2",
+    name="bio-matt-test3",
     version="0.1",
     author="Jeff Raffael Gower",
     author_email="jeffgower98@gmail.com",
